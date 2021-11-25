10ed99096eb2084e383a4117dde3d0148363aba1
Sabb95 <sabarish.science@gmail.com>
2021-11-25 13:44:04 +0100

    including Planck units

---
+++ Package_0.2.2/src/unitconvert/natural.py
@@ -0,0 +1,43 @@
+from unitconvert.getdimensions import *
+import astropy.units as u
+import astropy.constants as acon
+from collections import defaultdict
+
+def factorNatural(q):
+    dimdict = getdim(q.si)
+    a = dimdict[u.kg]
+    b = dimdict[u.m]
+    c = dimdict[u.s]
+    d = dimdict[u.A]
+    e = dimdict[u.K]
+    factorlist = {'hbar':1/2*(2*b + 2*c - d), 'c' : 1/2*(-4*a + 2*b  +d), 'k_B' : -e ,'eps0' : d/2  }
+    return factorlist
+
+def toNatural(q) :
+    q = q.si
+    dimdict = getdim(q)
+    a = dimdict[u.kg]
+    b = dimdict[u.m]
+    c = dimdict[u.s]
+    d = dimdict[u.A]
+    e = dimdict[u.K]
+    f = dimdict[u.cd]
+    g = dimdict[u.mol]
+    h = dimdict[u.rad]
+    factor = acon.hbar**(1/2*(2*b + 2*c - d)) * acon.c**(1/2*(-4*a + 2*b  +d)) * acon.k_B**(-e) * acon.eps0**(d/2)
+    natqu = q/factor
+    massdim = getdim(natqu)[u.kg]
+    try :
+        return natqu.to(u.eV**massdim * u.cd**f * u.mol**g * u.rad**h)
+    except :
+        return q
+
+def fromNatural(q,finalUnits) :
+    fac = factorNatural(finalUnits)
+    factor = acon.hbar**(fac['hbar']) * acon.c**(fac['c']) * acon.k_B**(fac['k_B']) * acon.eps0**(fac['eps0'])
+    try :
+        return (q*factor).to(finalUnits)
+    except :
+        return 'Cannot convert'
+    
+
